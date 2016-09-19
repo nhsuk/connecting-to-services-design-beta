83fe2f41edb440d10f76fad2be350477f0847334
Simon Wilson <simoncwilson@gmail.com>
2016-09-19 16:22:10 +0100

    More forms integration

--- app/assets/javascripts/nhsuk.js
+++ app/assets/javascripts/nhsuk.js
@@ -1,7 +1,10 @@
 const cookieMessage = require('./modules/cookie-message');
 const feedbackForm = require('./modules/feedback-form');
 const analytics = require('./modules/analytics');
+const selectionButtons = require('./modules/selection-buttons');
+
 
 cookieMessage('global-cookies-banner');
 feedbackForm.init();
 analytics.init();
+selectionButtons.init();
